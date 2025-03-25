7628b5682e5d5f74884fca3ae06011855c54e40a
ooikexin7 <ooi_ke_xin@students.edu.sg>
2025-03-25 09:08:52 +0000

    done

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -12,7 +12,10 @@ export class Game extends Scene {
         this.ballInMotion = false;
         this.cursors = null;
         this.wasd= null;
-
+        this.rightScoreText = null;
+        this.leftScoreText = null;
+        this.rightScore = 0;
+        this.leftScore = 0;
     }
 
     preload() {
@@ -27,8 +30,14 @@ export class Game extends Scene {
        this.ball.setCollideWorldBounds(true);
        this.ball.setBounce(1,1);
        this.input.keyboard.on("keydown-SPACE", this.startBall, this);
-        this.leftPaddle = this.add.image(50,HEIGHT/2, "paddle");
-        this.rightPaddle = this.add.image(974,HEIGHT/2, "paddle");
+        this.leftPaddle = this.physics.add.image(50,HEIGHT/2, "paddle");
+        this.rightPaddle = this.physics.add.image(974,HEIGHT/2, "paddle");
+
+
+        this.physics.add.collider(this.ball, this.leftPaddle, this.hitPaddle, null, this );
+        this.physics.add.collider(this.ball, this.rightPaddle, this.hitPaddle, null, this );
+        this.leftPaddle.setImmovable(true);
+        this.rightPaddle.setImmovable(true);
         this.cursors = this.input.keyboard.createCursorKeys();
         this.wasd = this.input.keyboard.addKeys(
             {
@@ -36,7 +45,8 @@ export class Game extends Scene {
                 down:Phaser.Input.Keyboard.KeyCodes.S
             }
         );
-
+this.leftScoreText = this.add.text(100,50 ,'0', {fontSize : '50px'});
+this.rightScoreText = this.add.text(924,50 ,'0', {fontSize : '50px'});
     }
 
     update() {
@@ -54,13 +64,38 @@ export class Game extends Scene {
        if(this.cursors.down.isDown && this.rightPaddle.y <=HEIGHT){
         this.rightPaddle.y +=5;
        }
+       const margin =30;
 
+     if(this.ball.x <margin){
+        this.rightScore +=1;
+        this.rightScoreText.setText(this.rightScore);
+        
+     
+          if(this.ball.x > (WIDTH-margin)){
+            this.leftScore += 1;
+            this.leftScoreText.setText(this.leftScore);
+         }
+    
+     }
     }
     startBall(){
+
         if(!this.ballInMotion){
             this.ball.setVelocity(200,200);
             this.ballInMotion = true;
+            
         }
+       hitPaddle()
+
+       
+    }
+    resetBall(){
+        this.ball.x = WIDTH/2;
+        this.ball.y = HEIGHT/2;
+        this.startBall();
+        this.ballInMotion = false;
+
     }
 
-}
+    }
+    
